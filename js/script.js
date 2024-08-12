c9201313f9e1ab4bce5da0d1af60ebfd3135ca49
putra23795 <gasinvinix@gmail.com>
2024-08-12 12:57:24 +0700

    create index

--- js/script.js
+++ js/script.js
@@ -1,1 +1,49 @@
-// javascript
+// // script.js
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     const form = document.querySelector("form");
+//     const outputDiv = document.querySelector(".output");
+  
+//     form.addEventListener("submit", function(event) {
+//       event.preventDefault();
+      
+//       // Ambil nilai input dari form
+//       const beratBadan = parseFloat(form.querySelector("input[placeholder='Berad Badan']").value);
+//       const tinggiBadan = parseFloat(form.querySelector("input[placeholder='Tinggi Badan']").value) / 100; // Convert cm to meters
+      
+//       if (isNaN(beratBadan) || isNaN(tinggiBadan)) {
+//         outputDiv.textContent = "Silakan masukkan berat badan dan tinggi badan yang valid.";
+//         return;
+//       }
+  
+//       // Hitung BMI
+//       const bmi = beratBadan / (tinggiBadan * tinggiBadan);
+//       let kategori = "";
+  
+//       // Tentukan kategori BMI
+//       if (bmi < 18.5) {
+//         kategori = "Berat badan kurang";
+//       } else if (bmi >= 18.5 && bmi < 24.9) {
+//         kategori = "Berat badan normal";
+//       } else if (bmi >= 25 && bmi < 29.9) {
+//         kategori = "Berat badan berlebih";
+//       } else {
+//         kategori = "Obesitas";
+//       }
+  
+//       // Tampilkan hasil
+//       outputDiv.innerHTML = `
+//         <h5>Hasil BMI</h5>
+//         <p>BMI Anda adalah: ${bmi.toFixed(2)}</p>
+//         <p>Kategori: ${kategori}</p>
+//       `;
+//     });
+  
+//     // Reset form
+//     const resetButton = form.querySelector(".bg-reset");
+//     resetButton.addEventListener("click", function() {
+//       form.reset();
+//       outputDiv.innerHTML = ""; // Kosongkan output ketika form direset
+//     });
+//   });
+  
